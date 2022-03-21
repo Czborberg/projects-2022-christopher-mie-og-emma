e1d82ff945e8ba54ea978dfa8dcf223cc46223a4
jkt210 <87707922+jkt210@users.noreply.github.com>
2022-03-21 16:32:46 +0100

    functions added

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,14 +1,14 @@
 def premium(q,p=0.2):
     """ 
-    premium function, pi
+    Function that captures the insurance company's premium policy 
     
     Args: 
-    q: input array
-    p: scalar (0.2 by default)
+    q: input array (coverage amount)
+    p: scalar (probability of monetary loss which is 0.2 by default)
     
-    Returns 
-    pi: output array
-    
+    Returns:
+    pi: output array (premium)
+
     """
     pi = p*q
     return pi
@@ -16,16 +16,34 @@ def premium(q,p=0.2):
 
 def utility(z,theta=-2):
     """ 
-    CRRA utility function, u(z)
+    CRRA utility function
     
     Args: 
-    z: input array
-    theta: scalar (-2 by default )
+    z: input array (what the agent values)
+    theta: scalar (constant that captures the agent's risk aversion which is -2 by default)
     
-    Returns 
-    u: output array
+    Returns:
+    u: output array (agent's utility)
     
     """
     u = z**(1+theta)/(1+theta)
     return u
+
+def expected_utility(q,x,p=0.2,y=1.0):
+    """ 
+    Function that calculates an insured agent's expected utility (for q=0 is holds for agents that are not insured)
+    
+    Args: 
+    q: input array (coverage amount)
+    x: input array (monetary loss)
+    p: scalar (probability of monetary loss which is 0.2 by default)
+    y: scalar (agent's assets which is 1.0 by default)
+    
+    Returns:
+    V: output array (expected utility)
+    
+    """
+    V = p*utility(y-x+q-premium(q)) + (1-p)*utility(y-premium(q))
+    return V
+
 
