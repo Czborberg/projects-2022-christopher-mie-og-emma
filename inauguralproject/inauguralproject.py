094890d6c3206d85c5c71452da4918bc348e237a
jkt210 <87707922+jkt210@users.noreply.github.com>
2022-03-21 15:26:19 +0100

    first commit

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,15 +1,31 @@
-def square(x):
-    """ square numpy array
+def premium(q,p=0.2):
+    """ 
+    premium function, pi
     
-    Args:
+    Args: 
+    q: input array
+    p: scalar (0.2 by default)
     
-        x (ndarray): input array
-        
-    Returns:
-    
-        y (ndarray): output array
+    Returns 
+    pi: output array
     
     """
+    pi = p*q
+    return pi
+
+
+def utility(z,theta=-2):
+    """ 
+    CRRA utility function, u(z)
     
-    y = x**2
-    return y
+    Args: 
+    z: input array
+    theta: scalar (-2 by default )
+    
+    Returns 
+    u: output array
+    
+    """
+    u = z**(1+theta)/(1+theta)
+    return u
+
